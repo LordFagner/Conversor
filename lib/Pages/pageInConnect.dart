93f46dc29012e58a9052ec63a8ebac479401d418
Dev_fag_corp <fagner.compras1.0.0@gmail.com>
2025-01-04 12:34:19 -0300

    Configurando o buildTextFiled , para futuras inclusões )

--- lib/Pages/pageInConnect.dart
+++ lib/Pages/pageInConnect.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Carregado extends StatefulWidget {
-
   Map<dynamic, dynamic>? cotacao;
 
   Carregado({super.key, required this.cotacao});
@@ -20,23 +19,53 @@ class _CarregadoState extends State<Carregado> {
     // TODO: implement initState
     super.initState();
     safeCotacao = widget.cotacao ?? {};
-    dolar = safeCotacao["results"]?["currencies"]?["USD"]?["buy"] ?? 0.0 ;
-    euro = safeCotacao["results"]?["currencies"]?["EUR"]?["buy"] ?? 0.0 ;
+    dolar = safeCotacao["results"]?["currencies"]?["USD"]?["buy"] ?? 0.0;
+    euro = safeCotacao["results"]?["currencies"]?["EUR"]?["buy"] ?? 0.0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.monetization_on),
-          Text(
-            "${euro}  ",
-            style: TextStyle(fontSize: 25, color: Colors.white),
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          spacing: 40,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.monetization_on,
+              color: Colors.amber,
+              size: 120,
+            ),
+            BuildTextFiled("Dolar", "\$"),
+            BuildTextFiled("Real", "R\$"),
+            BuildTextFiled("Euro", "\€"),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget BuildTextFiled(String text, String prefix) {
+  return TextField(
+    decoration: InputDecoration(
+      labelText: text,
+      labelStyle: TextStyle(
+        color: Colors.amber,
+        fontSize: 25,
+      ),
+      hoverColor: Colors.amber,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.amber, width: 2),
+      ),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 2, color: Colors.white)),
+      prefixText: prefix,
+      prefixStyle: TextStyle(
+          color: Colors.amber, fontSize: 25, decorationColor: Colors.amber),
+    ),
+    style: TextStyle(
+      color: Colors.amber,
+    ),
+  );
 }
